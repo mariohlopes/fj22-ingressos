1c178ced937d100657962e968b37f030a624bd41
Mario H Lopes <marionline90@gmail.com>
2018-09-22 12:08:04 -0300

    adicionado

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,10 +2,22 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
 	
 	public Sessao(LocalTime horario,Filme filme, Sala sala){
@@ -40,6 +52,9 @@ public class Sessao {
 	}
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+	public Sessao(){
+		
 	}
 	
 }
