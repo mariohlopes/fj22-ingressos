fbda6b9bc83a630acd1df05d65c1014e24847ed8
Mario H Lopes <marionline90@gmail.com>
2018-09-29 09:39:42 -0300

    segue

--- src/main/java/br/com/caelum/ingresso/model/Sala.java
+++ src/main/java/br/com/caelum/ingresso/model/Sala.java
@@ -1,18 +1,29 @@
 package br.com.caelum.ingresso.model;
 
-import org.hibernate.validator.constraints.NotBlank;
-
-import javax.persistence.*;
-import java.util.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 /**
  * Created by nando on 03/03/17.
  */
 @Entity
 public class Sala {
-
-    @Id
+	
+	@Id
     @GeneratedValue
     private Integer id;
 
@@ -21,6 +32,8 @@ public class Sala {
 
     @OneToMany(fetch = FetchType.EAGER)
     private List<Lugar> lugares = new ArrayList<>();
+    
+    private BigDecimal preco;
 
     /**
      * @deprecated hibernate only
@@ -72,5 +85,17 @@ public class Sala {
     public Integer lugar(String fileira, Integer posicao){
         Optional<Lugar> optional = this.lugares.stream().filter((x) -> fileira.equals(x.getFileira()) && posicao.equals(x.getPosicao())).findFirst();
         return optional.get().getId();
+    }
+    
+    public Sala(String nome, BigDecimal preco){
+    	this.nome = nome;
+    	this.preco = preco;
+    }
+    public BigDecimal getPreco(){
+    	return preco.setScale(2, RoundingMode.HALF_UP);
+    }
+    
+    public void setPreco(BigDecimal preco){
+    	this.preco = preco;
     }
 }
